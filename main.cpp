9ee3f5f9dfcf9c2ce91924cd60d9fcdaa9b6b235
The-RealOG <chibuezekingsley1234@gmail.com>
2025-04-11 16:43:16 -0500

    Most recent update

--- main.cpp
+++ main.cpp
@@ -1,26 +1,54 @@
+//imports the necessary libraries
 #include "rekognition.h"
 #include <iostream>
 #include <vector>
 #include <string>
+#include <algorithm>
+#include <cmath>
+#include <fstream>
+#include <filesystem>
 
 using namespace std;
 
 int main() {
-	Rekognition rekognition;
-	
-	vector<Rekognition::LabelInfo> rekognitionLabels;
-	
-	bool validFile;
-	string filename = "degu.jpg";
+	cout <<"** starting **" << endl;
 
-	rekognitionLabels = rekognition.getLabels(filename, validFile);
+	cout << "Enter a directory name (or '.' for current directory)" << endl;
+	string dirName;
+	cin >> dirName;
+	
+	//string filename = "degu.jpg";
+	//rekognitionLabels = rekognition.getLabels(filename, validFile);
+
+	//To confirm that the given directory actually exists
+	if (!filesystem::is_directory(dirName)) {
+		cout << "not a directory, exiting..." << endl;
+		return 0;
+	}
+	
+	//Iterate through each file in the provided directory
+	for (auto &entry : filesystem::directory_iterator(dirName)) {
+		if (filesystem::is_regular_file(entry)) {
+			string filename = entry.path().string();
+
+			//looks out for the ".jpg" extension
+			if (entry.path().extension()== ".jpg") {
+				cout << filename << endl;
+
+				//Initiate a rekognition object
+				Rekognition rekognition;
+				bool validFile;
+				vector<Rekognition::LabelInfo> rekognitionLabels = rekognition.getLabels(filename, validFile);
+				}
+			}
+		}
 
 	if (!validFile) {
 		cout << "invalid image file!" << endl;
 		return 1;
 		
 	} else {
-		for (const auto& labelInfo : rekognitionLabels) {
+		for (const auto &labelInfo : rekognitionLabels) {
 			cout << labelInfo.label << ": confidence of " << labelInfo.confidence << "%" << endl;
 		        }
 	}
